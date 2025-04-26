3a2363c625df065ecfaa71f7c0afbe4b9fc8aec4
joiii <joyeaudia752265@gmail.com>
2025-04-26 10:09:22 +0700

    Update income. TRN-OFF THE IP KEY

--- Public/income.js
+++ Public/income.js
@@ -3,17 +3,17 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
 // Firebase Config
+// Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyAU6CkWKfo2JuK6HW9dWNp_wafse0t4YUs",
-  authDomain: "nextgrowthgroup.firebaseapp.com",
-  databaseURL: "https://nextgrowthgroup-default-rtdb.firebaseio.com",
-  projectId: "nextgrowthgroup",
-  storageBucket: "nextgrowthgroup.firebasestorage.app",
-  messagingSenderId: "658734405364",
-  appId: "1:658734405364:web:2e11417e4465a53a90b0a1",
-  measurementId: "G-Y5DB2XL0TH"
+  apiKey: "RAHASIA PERUSAHA",
+  authDomain: "RAHASIA PERUSAHA",
+  databaseURL: "RAHASIA PERUSAHA",
+  projectId: "RAHASIA PERUSAHA",
+  storageBucket: "RAHASIA PERUSAHA",
+  messagingSenderId: "RAHASIA PERUSAHA",
+  appId: "RAHASIA PERUSAHA",
+  measurementId: "RAHASIA PERUSAHA",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
