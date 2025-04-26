562ae0b4c87fa43339c179f4d8975b0a5a93b025
DevonJonathan <165252324+DevonJonathan@users.noreply.github.com>
2025-04-27 02:54:33 +0700

    Delete login.js

--- login.js
+++
@@ -1,9 +0,0 @@
-function login() {
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-    console.log("Email: " + email + " Password: " + password);
-}
-
-function goToSignUp() {
-    window.location.href = 'signup.html'; // Redirects to Sign-Up Page
-}
